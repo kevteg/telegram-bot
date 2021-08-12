568e350c75db7b3be10723582aa29df3aee11ee5
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2021-08-12 18:34:43 +0530

    Formated the codes

--- foto.py
+++ foto.py
@@ -10,14 +10,14 @@ def nothing(*arg):
         pass
 
 def toma(camara):
-    i      = 0
+    i = 0
     cap = cv2.VideoCapture(camara)
     tim = time.time()
     while i < 6:
 	ret, img = cap.read()
         i = time.time() - tim
         #cv2.waitKey(100)
-        print i
+        print (i)
 	if i > 5:
   	   print "Whisky"
            cv2.imwrite("Foto.png", img)
